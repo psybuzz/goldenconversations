15abd103dc8d63c61651013721aa9fbe73ece38c
E <erikluo2@illinois.edu>
2014-07-01 01:39:57 -0500

    Update app and packages, added gitignore, new app.js

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-
 /**
- * Module dependencies.
+ * Golden Applications.
+ * 
+ * This file is the entry point to the main application.  It lists module dependencies, sets up
+ * environment variables, and starts a new server to listen to requests.
  */
 
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
+var messaging = require('./routes/messaging');
 var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars');
+var db = require('./db');
+
 
 var app = express();
 
@@ -27,7 +32,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/all', messaging.getMessage);
+app.post('/post', messaging.addMessage);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+// TODO(erik): Setup socket.io chat relay for realtime updates.
+// var io = require('socket.io')(http);
+
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+// });
