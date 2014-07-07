063baa65cba8cb6bf0875ec5a84edb2903ca1a6d
Nikil Selvam <nikil@ebookglue.com>
2014-07-06 22:26:56 -0700

    Established link between added posts with conversations and fetching posts with Q

--- routes/conversation.js
+++ routes/conversation.js
@@ -1,4 +1,8 @@
 var db = require('./../db.js');
+var Q = require('q');
+
+Conversation = db.models.Conversation;
+Post = db.models.Post;
 
 exports.create = function(req, res){
 	var conversation = new db.models.Conversation({
@@ -8,6 +12,8 @@ exports.create = function(req, res){
 	    isGroup             : req.body.isGroup,
 	    lastEdited          : Date.now()
 	});
+
+	console.log(req.body.question);
 
 	conversation.save(function(err){
 		if (err){
@@ -28,4 +34,38 @@ exports.update = function(req, res){
 	Conversation.findByIdAndUpdate(req.body.objectId, req.body.updata, function(err){
 		if (err) return console.log(err);
 	});
+};
+
+exports.getTestMessages = function(req, res){
+	// find all messages
+    Conversation.find( { question:"What is the best fruit in the world?" }, function (err, messages, count){
+    	if (err){
+			resError(res, "Could not find messages collection in DB.");
+		}
+
+		var results = [];
+		if (messages[0]){
+			console.log("messages[0]", messages[0]);
+			var jobs = messages[0].discussion.map(function(postId, index){
+				console.log("index is", index);
+				var d = Q.defer();
+				console.log("index is", index);
+					console.log("postId", postId);
+					Post.find({ _id: postId }, function(err, post){
+						if (err){
+							d.reject();
+						}
+						results[index] = post[0];
+						console.log("results in find is", results);
+						d.resolve(post);
+					});
+				return d.promise;
+			});
+			console.log("jobs", jobs);
+			Q.allSettled(jobs).then(function (){
+				console.log("results is", results);
+	    		res.send(results);	
+			})
+		}
+    });
 };
--- routes/post.js
+++ routes/post.js
@@ -1,19 +1,63 @@
 var db = require('./../db.js');
+var validator = require('validator');
+
+// sends response with an error message, and logs it in the console
+function resError(res, message){
+	console.error(message);
+	res.send({status: message, success: false});
+}
 
 exports.create = function(req, res){
-	var post = new db.models.Post({
-	    username	: req.body.username,
-	    content  	: req.body.content,
-	    time  		: Date.now()
-	});
+	// validate comment info
+	var name = req.body.username;
+	var content = req.body.content;
 
-	post.save(function(err){
-		if (err){
-			console.log(err);
-		} else{
-			res.send({status: 'OK', success: true});
-		}
-	});
+	if (typeof name === 'undefined' || typeof content === 'undefined'){
+		resError(res, "Missing data parameters (name, content)");
+	}
+
+	var cname = validator.toString(name);
+	content = validator.toString(content);
+	var validName = validator.isLength(cname, 1, 140) && (validator.isNull(cname) == false);
+	var validContent = validator.isLength(content, 140, 10000) && (validator.isNull(content) == false);
+	if (validName == false){
+		resError(res, "Invalid name");
+	} else if (validContent == false){
+		resError(res, "Invalid content");
+	} else {
+		// Create and save the post
+		var post = new db.models.Post({
+		    username	: req.body.username,
+		    content  	: req.body.content,
+		    time  		: Date.now()
+		});
+		post.save(function(err, message){
+			if (err){
+				resError(res, "Could not save message into db.");
+			} else {
+				// Add the post to the conversation's discussion and save
+			    Conversation.findOne( { _id: req.body.conversationId }, function (err, convo, count){
+			    	if (err){
+						resError(res, "Could not find messages collection in DB.");
+					}
+					else{
+						console.log("convo is");
+						console.log(convo);
+						console.log("post is", post);
+						console.log("post._id is ", post._id);
+						convo.discussion.push(post._id);
+				    	convo.save(function(err, message){
+				    		if (err){
+								resError(res, "Could not save message into db.");
+							} else {
+								res.send({status: 'OK', success: true});
+							}
+						});
+					}
+			    });
+			}
+		});
+	}
 };
 
 exports.delete = function(req, res){
