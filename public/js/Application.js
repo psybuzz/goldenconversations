ec6b400fede9380b7b05e23eff54cd8333cdb2ec
E <erikluo2@illinois.edu>
2014-07-27 15:54:15 -0500

    Fixed up de-duplication issues

--- public/js/Application.js
+++ public/js/Application.js
@@ -17,37 +17,6 @@ $(document).ready(function() {
 	});
 });
 
-union = function () {
-    debugger
-    var args = Array.prototype.slice.call(arguments, 0);
-    hash = {};
-    union = [];
-
-    if (typeof args[1] === 'function'){
-        var items = args[0];
-        var hashes = items.map(args[1]);
-        for (var i = 0; i < items.length; i++) {
-            if (hash[hashes[i]] !== true){
-                hash[hashes[i]] = true;
-                union.push(items[i]);
-            }
-        }
-
-    } else {
-        for (var arg = 0; arg < args.length; arg++) {
-            for (var i = 0, len = args[arg].length; i < len; i++) {
-                var item = args[arg][i];
-                if (hash[item] !== true){
-                    hash[item] = true;
-                    union.push(item);
-                }
-            }
-        }
-    }
-
-    return union;
-}
-
 $('#mainform').on('submit', function (e){
 	e.preventDefault();
 
