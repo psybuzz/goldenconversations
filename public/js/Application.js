37faacdc801b026c6ba8bad37fdaab2ab8a8fd5d
Tony Jing <tonyxjing@gmail.com>
2014-08-26 00:14:58 -0700

    updating PR based on comments

--- public/js/Application.js
+++ public/js/Application.js
@@ -1,4 +1,4 @@
-function peopleTypeEvent () {
+function peopleTypeEvent(){
     var q = $('#people_input').val();
     if (q == ''){
         $('#search_names').html('');
@@ -17,13 +17,13 @@ function peopleTypeEvent () {
 $('#people_input').keyup(_.debounce(peopleTypeEvent, 150));
 
 var addedPeople = [];
-$('#search_names').on('click', '.name', function (e) {
+$('#search_names').on('click', '.name', function(e){
     var id = $(this).attr('id').slice(5);
 
     addedPeople.push({'_id': id});
 })
 
-$('#new-input-modal button[type=submit]').click(function(evt) {
+$('#new-input-modal button[type=submit]').click(function(evt){
     evt.preventDefault();
 
     // Remove the modal for now.
@@ -31,52 +31,58 @@ $('#new-input-modal button[type=submit]').click(function(evt) {
     document.getElementById('lean_overlay').style.display = 'none';
 });
 
-function styleNewNames() {
-    $('.userShape').each(function(){
-        var $this = $(this);
-        var originalNameText = $this.text();
+function styleNewNames(styleSelection){
+    styleSelection.each(function (){
+        var $currentShape = $(this);
+        var originalNameText = $currentShape.text();
         var colorValue;
 
         $('.name').each(function(){
-            var $this = $(this);
-            var storedName = $this.text();
+            var $currentName = $(this);
+            var storedName = $currentName.text();
             if (originalNameText === storedName) {
-                colorValue = $this.find('.name-colors').css('background-color');
+                colorValue = $currentName.find('.name-colors').css('background-color');
             }
         });
 
-        var initials = $this.text()
+        var initials = $currentShape.text()
                             .split(' ')
                             .map(function(s){ return s.charAt(0);})
                             .join('');
-        $this.html(initials);
-        $this.prepend('<div class="color-ball"></div>');
-        $this.find('.color-ball').css({
-            width: '15px',
-            height: '15px',
-            display: 'inline-block',
+        $currentShape.html(initials);
+        $currentShape.prepend('<div class="color-ball"></div>');
+        $currentShape.find('.color-ball').css({
             backgroundColor: colorValue,
-            borderRadius: '100%',
-            margin: '0 7px'
         });
     });
 }
 
-
-$(document).ready(function() {
+$(document).ready(function(){
     var socket = io();
 
 	// Load comments
-	$.get('/conversation/posts', {conversationId: conversationId}, function (data) {
+	$.get('/conversation/posts', {conversationId: conversationId}, function(data) {
         if (data.success){
     		var text = '';
     		for (var i = 0; i < data.message.length; i++) {
-                text += '<span class="userShape" data_id="' + data.message[i].userid + '">' + data.message[i].username + '</span>';
-    			text += '<p>' + data.message[i].content + '</p><br>';
+                var colorValue;
+                $('.name').each(function(){
+                    var $currentName = $(this);
+                    var storedName = $currentName.text();
+                    if (data.message[i].username === storedName) {
+                        colorValue = $currentName.find('.name-colors').css('background-color');
+                    }
+                });
+
+                var initials = data.message[i].username
+                            .split(' ')
+                            .map(function(s){ return s.charAt(0);})
+                            .join('');
+
+                text += '<span class="userShape" data_id="' + data.message[i].userid + '"><div class="color-ball" style="background-color:'+ colorValue +';"></div>' + initials + '</span>';
+    			text += '<p>' + data.message[i].content + '</p><br>';    
     		};
     		$('.display-area').append(text);
-            
-            styleNewNames();
             setScrollPos();
         } else if (data.redirect){
             window.location.href = data.redirect;
@@ -84,7 +90,7 @@ $(document).ready(function() {
 	});
 });
 
-$('#mainform').on('submit', function (e){
+$('#mainform').on('submit', function(e){
 	e.preventDefault();
 
 	$.ajax({
@@ -108,52 +114,23 @@ $('#mainform').on('submit', function (e){
 		alert('Send failed.');
 	}
 
-    function onSubmitComment(data) {
+    function onSubmitComment(data){
     	if (data.success){
         	$('.display-area').append('<span class="userShape" data_id="' +  userId + '">' + 
                     user + '</span><p>' + message + '</p><br>');
             setScrollPos();
             $('textarea').val("");
             
-            $('.userShape:last-of-type').each(function(){
-                var $this = $(this);
-                var originalNameText = $this.text();
-                var colorValue;
-
-                $('.name').each(function(){
-                    var $this = $(this);
-                    var storedName = $this.text();
-                    if (originalNameText === storedName) {
-                        colorValue = $this.find('.name-colors').css('background-color');
-                    }
-                });
-
-                var initials = $this.text()
-                                    .split(' ')
-                                    .map(function(s){ return s.charAt(0);})
-                                    .join('');
-                $this.html(initials);
-                $this.prepend('<div class="color-ball"></div>');
-                $this.find('.color-ball').css({
-                    width: '15px',
-                    height: '15px',
-                    display: 'inline-block',
-                    backgroundColor: colorValue,
-                    borderRadius: '100%',
-                    margin: '0 7px'
-                });
-            });
+            styleNewNames($('.userShape:last-of-type'));
             
     	} else {
     		alert('Nein. Write more. Unless you have written more than 10000 characters. Write less in that case.');
     	}
     }    
-
-    
 });
 
 // Delete conversation button
-$('#deleteConvo').click(function (){
+$('#deleteConvo').click(function(){
     if (confirm('Are you sure you want to be rid of this nasty conversation forever?')){
         $.ajax({
             type: "POST",
@@ -163,7 +140,7 @@ $('#deleteConvo').click(function (){
                 conversationId: conversationId,
                 _csrf: token
             },
-            success: function (data){
+            success: function(data){
                 if (data.success){
                     alert('Successfully deleted your conversation!');
                     window.location.href = data.redirect;
